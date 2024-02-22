30870fa2a8833a13f77c546c459c1586c1f21de6
Admin <admin@Admins-MacBook-Air.local>
2024-02-22 12:00:51 +0530

    division feature

--- main.java
+++ main.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 class main
 {
+    public static int division(int add)
+    {
+        int x=add/2;
+        return x;
+    }
+
     
     public static void  main(String[] args)
     {
